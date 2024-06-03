44bd8869e336b632ffd88244d7d325106681e405
Barath1619 <barath1619a@gmail.com>
2024-06-03 12:18:11 +0100

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+    <div className="main">
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon}/>
+        </div>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Barath.</span></p>
+                <p><span>How can I help you today? </span></p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Act like Mowgli from The Jungle Book. Answer this question: What's your favorite memory with Baloo?</p>
+                    <img src={assets.compass_icon}/>
+                </div>
+                <div className="card">
+                    <p>Help me finish my gaming podcast tagline: play, win, and ....</p>
+                    <img src={assets.bulb_icon}/>
+                </div>
+                <div className="card">
+                    <p>Outline a way to home routine: organizing my closet</p>
+                    <img src={assets.message_icon}/>
+                </div>
+                <div className="card">
+                    <p>Draft an email to a recruiter to accept a job offer</p>
+                    <img src={assets.code_icon}/>
+                </div>
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Main
