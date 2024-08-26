04bc3bd93bf5235ba1c6dd8ed137cacf7a187adc
Barath1619 <barath1619a@gmail.com>
2024-08-26 22:30:15 +0100

    genini working

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Main.css";
 import { assets } from "../../assets/assets";
+import { context } from "../../context/context";
 
 const Main = () => {
+  const {
+    onSend,
+    recentPrompt,
+    showResult,
+    loading,
+    resultData,
+    setInput,
+    input,
+  } = useContext(context);
+
   return (
     <div className="main">
       <div className="nav">
@@ -10,40 +21,77 @@ const Main = () => {
         <img src={assets.user_icon} />
       </div>
       <div className="main-container">
-        <div className="greet">
-          <p>
-            <span>Hello, Barath Kumar.</span>
-          </p>
-          <p>How can I help you today? </p>
-        </div>
-        <div className="cards">
-          <div className="card">
-            <p>
-              Act like Mowgli from The Jungle Book. Answer this question: What's
-              your favorite memory with Baloo?
-            </p>
-            <img src={assets.compass_icon} />
+        {!showResult ? (
+          <>
+            <div className="greet">
+              <p>
+                <span>Hello, Barath Kumar.</span>
+              </p>
+              <p>How can I help you today? </p>
+            </div>
+            <div className="cards">
+              <div className="card">
+                <p>
+                  Act like Mowgli from The Jungle Book. Answer this question:
+                  What's your favorite memory with Baloo?
+                </p>
+                <img src={assets.compass_icon} />
+              </div>
+              <div className="card">
+                <p>
+                  Help me finish my gaming podcast tagline: play, win, and ....
+                </p>
+                <img src={assets.bulb_icon} />
+              </div>
+              <div className="card">
+                <p>Outline a way to home routine: organizing my closet</p>
+                <img src={assets.message_icon} />
+              </div>
+              <div className="card">
+                <p>Draft an email to a recruiter to accept a job offer</p>
+                <img src={assets.code_icon} />
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="result">
+            <div className="result-title">
+              <img src={assets.user_icon} />
+              <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+              <img src={assets.gemini_icon} />
+              {loading ? (
+                <div className="loader">
+                  <hr />
+                  <hr />
+                  <hr />
+                </div>
+              ) : (
+                <p dangerouslySetInnerHTML={{ __html: resultData }}></p>
+              )}
+            </div>
           </div>
-          <div className="card">
-            <p>Help me finish my gaming podcast tagline: play, win, and ....</p>
-            <img src={assets.bulb_icon} />
-          </div>
-          <div className="card">
-            <p>Outline a way to home routine: organizing my closet</p>
-            <img src={assets.message_icon} />
-          </div>
-          <div className="card">
-            <p>Draft an email to a recruiter to accept a job offer</p>
-            <img src={assets.code_icon} />
-          </div>
-        </div>
+        )}
+
         <div className="main-bottom">
           <div className="search-box">
-            <input type="text" placeholder="Enter a prompt here..." />
+            <input
+              onChange={(event) => setInput(event.target.value)}
+              value={input}
+              type="text"
+              placeholder="Enter a prompt here..."
+            />
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
+              <img
+                onClick={() => {
+                  onSend(input);
+                }}
+                src={assets.send_icon}
+                alt=""
+              />
             </div>
           </div>
           <p className="bottom-info">
--- src/context/context.jsx
+++ src/context/context.jsx
@@ -1,24 +1,53 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
 
 export const context = createContext();
 
 const ContextProvider = ({ children }) => {
   const [input, setInput] = useState("");
-  const [resentPromt, setResentPromt] = useState("");
-  const [previousPromt, setPreviousPromt] = useState([]);
+  const [recentPrompt, setRecentPrompt] = useState("");
+  const [previousPrompt, setPreviousPrompt] = useState([]);
   const [showResult, setShowResult] = useState("");
   const [loading, setLoading] = useState(false);
   const [resultData, setResultData] = useState("");
 
+  const delayPara = (index, nextword) => {
+    setTimeout(function () {
+      setResultData((prev) => prev + nextword);
+    }, 75 * index);
+  };
+
   const onSend = async (prompt) => {
-    await run(prompt);
+    setShowResult("");
+    setLoading(true);
+    setShowResult(true);
+    setRecentPrompt(input);
+    setPreviousPrompt((prev) => [...prev, input]);
+    const response = await run(prompt);
+    let responseArray = response.split("**");
+    let newResponse;
+    for (let i = 0; i < responseArray.length; i++) {
+      if (i === 0 || i % 2 !== 1) {
+        newResponse += responseArray[i];
+      } else {
+        newResponse += "<b>" + responseArray[i] + "</b>";
+      }
+    }
+    let newResponse2 = newResponse.split("*").join("</br>");
+
+    let newResponseArray = newResponse2.split(" ");
+    for (let i = 0; i < newResponseArray.length; i++) {
+      const nextword = newResponseArray[i];
+      delayPara(i, nextword + " ");
+    }
+    setLoading(false);
+    setInput("");
   };
   const contextValue = {
-    previousPromt,
-    setPreviousPromt,
-    resentPromt,
-    setResentPromt,
+    previousPrompt,
+    setPreviousPrompt,
+    recentPrompt,
+    setRecentPrompt,
     showResult,
     setShowResult,
     onSend,
@@ -28,12 +57,8 @@ const ContextProvider = ({ children }) => {
     setInput,
   };
 
-  onSend("tell me about shares today?");
-  return (
-    <context.Provider value={{ contextValue, onSend }}>
-      {children}
-    </context.Provider>
-  );
+  //onSend("tell me about shares today?");
+  return <context.Provider value={contextValue}>{children}</context.Provider>;
 };
 
 export default ContextProvider;
